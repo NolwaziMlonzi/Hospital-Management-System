297a8d7652de87d35120ea9478fb897c63d763f4
Nolwazi <nolwazi.mlonzi@geniianalytics.com>
2022-01-15 13:26:10 +0200

    new commit

--- Startup.cs
+++ Startup.cs
@@ -4,10 +4,12 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System.Web.Optimization;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BundlerMinifier.TagHelpers;
 
 namespace Hospital_Management_System
 {
